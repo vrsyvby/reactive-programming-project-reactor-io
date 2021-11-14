d5ac66ad993123d0df2b3ad6e3fbc4dfdcc87d1b
vrsyvby@gmail.com <3%@tatime>
2021-11-14 17:29:45 +0530

    Flux generator baby step2

--- reactive-programming-using-reactor-main/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor-main/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -12,10 +13,14 @@ public class FluxAndMonoGeneratorService {
         return Flux.fromIterable(List.of("alex","bob","chloe","adam"));
     }
 
+    public Mono<Double> namesMono(){
+
+        return Mono.fromSupplier(Math::random);
+    }
+
     public static void main(String[] args) {
         FluxAndMonoGeneratorService service= new FluxAndMonoGeneratorService();
-        service.namesFlux().subscribe(System.out::println);
-
-
+        service.namesFlux().subscribe(System.out::println);// nothing happens until you subscribe.
+        service.namesMono().subscribe(System.out::println);
     }
 }
